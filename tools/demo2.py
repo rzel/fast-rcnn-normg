b8cf6489e0a6101bfa5dc6888fffb52b6d349e08
xiaolonw <xiaolonw@cs.cmu.edu>
2016-03-06 07:04:13 -0500

    test

--- tools/demo2.py
+++ tools/demo2.py
@@ -161,7 +161,8 @@ if __name__ == '__main__':
     print '\n\nLoaded network {:s}'.format(caffemodel)
 
     for i in xrange(sample_num):
-        image_name = 'img_{:4d}'.format(i)
+        print(i)
+        image_name = 'img_{04d}'.format(i)
         save_name = os.path.join(savedir, image_name + '.txt') 
         demo(net, image_name, detclass, ssdir, imgdir, save_name)
 
